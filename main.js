8d74af49b39351bafb36b76fad4dff1d0b3120e4
Mauricio Raupp <mauricio.p.raupp@gmail.com>
2024-08-07 14:50:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,124 +1,247 @@
-const questions = [
+questions = [
+
     {
-        question: "O que é deficiência intelectual?",
+
+        question: "Qual é o objetivo principal do Artigo 7º da Constituição Federal?",
+
         answers: [
-            "Uma condição que afeta apenas a memória.",
-            "Uma condição que afeta a capacidade de aprender e entender.",
-            "Uma condição que afeta apenas a capacidade de se comunicar verbalmente.",
+
+            "Regular o comércio exterior.",
+
+            "Garantir direitos e garantias para os trabalhadores.",
+
+            "Estabelecer regras para a administração pública.",
+
         ],
+
         true_answer: 1,
+
     },
+
     {
-        question: "Quais são algumas causas comuns de deficiência intelectual?",
+
+        question: "O Artigo 7º da Constituição Federal garante o direito a férias anuais remuneradas. Qual é a duração mínima dessas férias?",
+
         answers: [
-            "Exposição a altas temperaturas.",
-            "Fatores genéticos ou lesões cerebrais durante o nascimento.",
-            "Falta de atividade física.",
+
+            "10 dias.",
+
+            "20 dias.",
+
+            "30 dias.",
+
         ],
-        true_answer: 1,
-    },
-    {
-        question: "Qual é a diferença entre deficiência intelectual e deficiência de desenvolvimento?",
-        answers: [
-            "Não há diferença, são termos intercambiáveis.",
-            "Deficiência de desenvolvimento é uma categoria mais ampla.",
-            "Deficiência intelectual é uma categoria mais ampla.",
-        ],
+
         true_answer: 2,
+
     },
+
     {
-        question: "Quais são alguns sinais comuns de deficiência intelectual em crianças?",
+
+        question: "De acordo com o Artigo 7º, qual é o direito garantido aos trabalhadores em relação ao descanso semanal?",
+
         answers: [
-            "Rápido desenvolvimento da linguagem e habilidades motoras.",
-            "Dificuldade em compreender instruções simples.",
-            "Hiperfoco em uma única área de interesse.",
+
+            "Um dia de descanso a cada 15 dias.",
+
+            "Um dia de descanso a cada 7 dias.",
+
+            "Um dia de descanso a cada 5 dias.",
+
         ],
+
         true_answer: 1,
+
     },
+
     {
-        question: "Qual é uma abordagem eficaz no tratamento de pessoas com deficiência intelectual?",
+
+        question: "Qual é a duração mínima da licença-maternidade garantida pelo Artigo 7º da Constituição?",
+
         answers: [
-            "Exclusão social para evitar sobrecarga sensorial.",
-            "Intervenção precoce e apoio personalizado.",
-            "Minimização do acesso a recursos educacionais.",
+
+            "60 dias.",
+
+            "90 dias.",
+
+            "120 dias",
+
         ],
-        true_answer: 1,
+
+        true_answer: 2,
+
     },
+
     {
-        question: "Como as deficiências intelectuais podem afetar a vida diária?",
+
+        question: "O Artigo 7º estabelece que o trabalhador deve receber uma remuneração proporcional ao tempo trabalhado em caso de demissão. Qual é o nome dessa remuneração?",
+
         answers: [
-            "Elas não têm impacto significativo na vida diária.",
-            "Podem tornar tarefas cotidianas mais desafiadoras.",
-            "Aumentam a independência e a autonomia.",
+
+            "Indenização.",
+
+            "Seguro-desemprego",
+
+            "Fundo de Garantia do Tempo de Serviço (FGTS).",
+
         ],
-        true_answer: 1,
+
+        true_answer: 2,
+
     },
+
     {
-        question: "Qual é uma estratégia importante para apoiar a inclusão de pessoas com deficiência intelectual na comunidade?",
+
+        question: "O Artigo 7º garante o direito à aposentadoria. Qual é a principal característica desse direito?",
+
         answers: [
-            "Isolamento social para evitar situações estressantes.",
-            "Fornecer oportunidades de educação inclusiva e participação em atividades.",
-            "Limitar o acesso a ambientes sociais.",
+
+            "Aposentadoria integral com base em tempo de serviço.",
+
+            "Aposentadoria proporcional com base em contribuições.",
+
+            "Aposentadoria opcional com base em idade.",
+
         ],
-        true_answer: 1,
-    },
-    {
-        question: "Quais são alguns mitos comuns sobre deficiência intelectual?",
-        answers: [
-            "Pessoas com deficiência intelectual são sempre incapazes de aprender.",
-            "Pessoas com deficiência intelectual são sempre talentosas em todas as áreas.",
-            "Deficiência intelectual é uma escolha pessoal.",
-        ],
+
         true_answer: 0,
+
     },
+
     {
-        question: "Qual é a importância do apoio da família para pessoas com deficiência intelectual?",
+
+        question: "Qual é o direito garantido pelo Artigo 7º em relação ao trabalho noturno?",
+
         answers: [
-            "O apoio da família não é significativo.",
-            "O apoio da família pode ajudar a promover independência e autoestima.",
-            "O apoio da família pode causar mais estresse e ansiedade.",
+
+            "Jornada reduzida.",
+
+            "Aumento de 50% no salário."
+
+            "Descanso compensatório.",
+
         ],
+
         true_answer: 1,
+
     },
+
     {
-        question: "Quais são alguns recursos disponíveis para pessoas com deficiência intelectual?",
+
+        question: "O Artigo 7º assegura aos trabalhadores o direito a um ambiente de trabalho seguro e saudável. Qual é a principal medida para garantir essa segurança?",
+
         answers: [
-            "Acesso limitado a educação e serviços de saúde.",
-            "Serviços de apoio, terapias e programas de inclusão social.",
-            "Nenhum recurso disponível.",
+
+            "Monitoramento contínuo.",
+
+            "Equipamentos de proteção individual.",
+
+            "Treinamento apenas para funcionários novos.",
+
         ],
+
         true_answer: 1,
+
     },
+
+    {
+
+        question: "De acordo com o Artigo 7º, quais são os direitos garantidos aos trabalhadores urbanos e rurais?",
+
+        answers: [
+
+            "Apenas os direitos básicos.",
+
+            "Os mesmos direitos, com diferenças nas condições de trabalho.",
+
+            "Direitos exclusivos para trabalhadores urbanos.",
+
+        ],
+
+        true_answer: 1,
+
+    },
+
+    {
+
+        question: "O Artigo 7º assegura que os trabalhadores têm direito a um seguro contra acidentes de trabalho. Qual é o nome desse seguro?",
+
+        answers: [
+
+            "Seguro-desemprego",
+
+            "Previdência social.",
+
+            "Seguro de acidentes de trabalho.",
+
+        ],
+
+        true_answer: 1,
+
+    },
+
 ];
 
+
+
 const quiz = document.querySelector('#quiz')
+
 const template = document.querySelector('template')
 
+
+
 const true_answers = new Set()
+
 const totalQuestions = questions.length
+
 const showScore = document.querySelector('#score span')
 
+
+
 for(const item of questions) {
+
     const quizItem = template.content.cloneNode(true)
+
     quizItem.querySelector('h3').textContent = item.question
 
+
+
     for(const answer of item.answers) {
+
         const dt = quizItem.querySelector('dl dt').cloneNode(true)
+
         dt.querySelector('span').textContent = answer
+
         dt.querySelector('input').setAttribute('name', 'question-' + questions.indexOf(item))
+
         dt.querySelector('input').value = item.answers.indexOf(answer)
+
         dt.querySelector('input').onchange = (event) => {
+
             const isTrue = event.target.value == item.true_answer
 
+
+
             true_answers.delete(item)
+
             if(isTrue) {
+
                 true_answers.add(item)
+
             }
+
             showScore.textContent = true_answers.size + ' de ' + totalQuestions
+
         }
 
+
+
         quizItem.querySelector('dl').appendChild(dt)
+
     }
+
     quizItem.querySelector('dl dt').remove()
+
     quiz.appendChild(quizItem)
-}
+
+    }
